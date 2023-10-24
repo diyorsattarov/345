a319129d4321cb1567b04985ee4d26da385fc964
diyooor <diyorsattarov7@gmail.com>
2023-10-23 22:04:15 -0500

    Added navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,7 @@
 import './App.css';
 import { Flowbite } from 'flowbite-react';
-// import Timeline from './components/Timeline/Timeline';
 import Header from './components/Header/Header';
-// import Footer from './components/Footer/Footer';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
 
 function App() {
@@ -12,7 +11,7 @@ function App() {
   const toggleDarkMode = () => {
     const newDarkMode = !darkMode;
     setDarkMode(newDarkMode);
-    
+
     // Save the dark mode state to local storage
     localStorage.setItem('darkMode', newDarkMode);
   };
@@ -21,13 +20,43 @@ function App() {
     // Apply the dark mode class to the body
     document.body.classList.toggle('dark', darkMode);
   }, [darkMode]);
+
   return (
     <>
       <Flowbite>
-      <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+        <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+        <Router>
+          <Switch>
+            <Route path="/" exact>
+              {/* The component to render for the home page */}
+              <Home />
+            </Route>
+            <Route path="/about">
+              {/* The component to render for the about page */}
+              <About />
+            </Route>
+            <Route path="/contact">
+              {/* The component to render for the contact page */}
+              <Contact />
+            </Route>
+          </Switch>
+        </Router>
       </Flowbite>
     </>
-  )
+  );
 }
 
-export default App
+// Define your individual page components, for example:
+function Home() {
+  return <div>Home Page Content</div>;
+}
+
+function About() {
+  return <div>About Page Content</div>;
+}
+
+function Contact() {
+  return <div>Contact Page Content</div>;
+}
+
+export default App;
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -5,10 +5,24 @@ import './Header.css';
 function Header({ darkMode, toggleDarkMode }) {
   return (  
     <div className={`header ${darkMode ? 'dark' : ''}`}>
-      <div className="header-content"> {/* Create a container for centering */}
+      <div className="header-content">
+        <NavigationBar darkMode={darkMode} />
         <DarkThemeToggle onClick={toggleDarkMode} className={darkMode ? 'dark' : ''} />
       </div>
     </div>
+  );
+}
+
+function NavigationBar({ darkMode }) {
+  return (
+    <nav className={`navigation-bar ${darkMode ? 'dark' : ''}`}>
+      {/* Add your navigation links here */}
+      <ul className="nav-list">
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </nav>
   );
 }
 
